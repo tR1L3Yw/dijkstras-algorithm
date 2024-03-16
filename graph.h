d12cdf153d59b2f2d7dee8715dbe79928cb4e302
tR1L3Yw <163371417+tR1L3Yw@users.noreply.github.com>
2024-03-16 11:43:27 -0500

    bedder hedder
    also convention and optimization
    oops nvm i already did my ifndefies
    STILL NEEDS COMMENTS AND FINAL TEST

--- graph.h
+++ graph.h
@@ -3,13 +3,18 @@
 #include <string>
 #include <climits>
 
-using namespace std;
+#ifndef GRAPH_H
+#define GRAPH_H
 
-struct Edge{
+#include<string>
+
+using namespace std;
+struct Edge {
+    //std::
     string source;
     string destination;
     int weight;
-
+    
     Edge()
     {
         source = "";
@@ -17,7 +22,7 @@ struct Edge{
         weight = 0;
     }
 
-    Edge( string src, string dst, int wt)
+    Edge( string src, string dst, int wt )
     {
         source = src;
         destination = dst;
@@ -31,6 +36,8 @@ struct Edge{
         weight = e2->weight;
     }
 };
+
+#endif
 
 struct VertexTrack{
     string name;
@@ -79,179 +86,48 @@ struct Node {
 
 class MinHeap {
 private:
+    
     Node* heap;
     int capacity;
     int size;
     
+    void swapNodes(int i, int j);
+    void siftUp(int i);
+    void siftDown(int i);
+    
     int parent(int i) { return (i - 1) / 2; }
     int leftChild(int i) { return 2 * i + 1; }
     int rightChild(int i) { return 2 * i + 2; }
-    void swapNodes(int i, int j) {
-        Node temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-    }
-    void siftUp(int i) {
-        while (i > 0 && heap[parent(i)].distance > heap[i].distance) {
-            swapNodes(i, parent(i));
-            i = parent(i);
-        }
-    }
-    void siftDown(int i) {
-        int minIndex = i;
-        int l = leftChild(i);
-        if (l < size && heap[l].distance < heap[minIndex].distance) {
-            minIndex = l;
-        }
-        int r = rightChild(i);
-        if (r < size && heap[r].distance < heap[minIndex].distance) {
-            minIndex = r;
-        }
-        if (i != minIndex) {
-            swapNodes(i, minIndex);
-            siftDown(minIndex);
-        }
-    }
     
 public:
-    MinHeap(int cap) {
-        heap = new Node[cap];
-        capacity = cap;
-        size = 0;
-    }
-    ~MinHeap() {
-        delete[] heap;
-    }
     
-    void insert(Node node) {
-        if (size == capacity) {
-            return;
-        }
-        heap[size] = node;
-        size++;
-        siftUp(size - 1);
-    }
-    Node extractMin() {
-        Node minNode = heap[0];
-        heap[0] = heap[size - 1];
-        size--;
-        siftDown(0);
-        return minNode;
-    }
-    bool isEmpty() {
-        return size == 0;
-    }
-};
+    MinHeap(int cap);
+    ~MinHeap();
 
+    void insert(Node node);
+    Node extractMin();
+    bool isEmpty();
+    
+};
+    
 class graphDW{
 private:
+    
     int numVertices;
     int buildIndex;
     Vertex** graphVertices;
-
+    
 public:
-    graphDW( int nV )
-    {
-        numVertices = nV;
-        buildIndex = 0;
-        graphVertices = new Vertex*[nV];
-    }
-
-    void addVertex( Vertex* inVertex )
-    {
-        graphVertices[buildIndex] = inVertex;
-        buildIndex++;
-    }
-
-    Vertex* getVertex( int index ) 
-    {
-        return graphVertices[index];
-    }
-
-    int getVertexIndex( string name )
-    {
-        int index;
-        for( int i = 0; i < numVertices; i++ )
-        {
-            if( graphVertices[i]->id == name )
-            {
-                index = i;
-            }
-        }
-        return index;
-    }
-
-    void dijkstrasAlgorithm ( string startVertex, string endVertex )
-    {
-        int startIndex = this->getVertexIndex(startVertex);
-        int endIndex = this->getVertexIndex(endVertex);
-
-        this->getVertex(startIndex)->distance = 0;
-
-        Vertex* start = this->getVertex(startIndex);
-
-        Node initial(start, 0);
-
-        MinHeap pq(numVertices);
-
-        pq.insert(initial);
-
-
-
-        while(!pq.isEmpty())
-        {
-            Node extract = pq.extractMin();
-
-            Vertex* current = extract.vertex;
-            current->seen = true;
-
-            for( int i = 0; i < current->outDegree; i++)
-            {
-                int adjacentIndex = this->getVertexIndex(current->edges[i]->destination);
-
-                //cout << adjacentIndex << endl;
-
-                Vertex* adjacent = this->getVertex(adjacentIndex);
-
-                if( !adjacent->seen )
-                {
-                    int tempDistance = current->distance + current->edges[i]->weight;
-
-                    if( tempDistance < adjacent->distance )
-                    {
-                        adjacent->distance = tempDistance;
-                        adjacent->lastJump = current;
-                    }
-
-                    if( adjacent->id == endVertex )
-                    {
-                        //cout << adjacent->id << " " << adjacent->distance << endl;
-                        break;
-                    }
-
-                    Node fresh(adjacent, adjacent->distance);
-                    pq.insert(fresh);
-                }
-            }
-        }
-
-        string path = "";
-
-        Vertex* target = this->getVertex(endIndex);
-        int cost = target->distance;
-
-        while( target->lastJump != nullptr )
-        {
-            path = target->id + " " + path;
-            target = target->lastJump;
-        }
-
-        path = startVertex + " " + path;
-
-        cout << "Path: " << path << endl;
-        cout << "Cost: " << cost << endl;
-
-    }    
-
+    
+    graphDW( int nV );
+    
+    void addVertex( Vertex* inVertex );
+    
+    Vertex* getVertex( int index );
+    
+    int getVertexIndex( string name );
+    
+    void dijkstrasAlgorithm ( string startVertex, string endVertex );
+    
 };
 
