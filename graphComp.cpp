d50ff420401fcfcb703a62f99923e08f991879b9
tR1L3Yw <163371417+tR1L3Yw@users.noreply.github.com>
2024-03-24 15:21:22 -0500

    fresh graphComp.cpp

--- graphComp.cpp
+++ graphComp.cpp
@@ -1,77 +1,33 @@
+#include "graphComp.cpp"
+#include "MinHeap.cpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <climits>
 
-// BEGIN Edge struct
 
-struct Edge {
+class graphDW {
+private:
     // attributes
-    std::string source;        // source and destination tags to indicate direction
-    std::string destination;
-    int weight;                // weight in this application denotes distance
+    int numVertices;        // vertex count
+    int buildIndex;         // vertex track relative to graph
+    Vertex** graphVertices; // dynamic array representing graph via Vertex pointers
+    
+public:
+    // parameterized constructor
+    graphDW( int nV );
 
-    // default constuctor
-    Edge() {
-        source = "";           
-        destination = "";
-        weight = 0;
-    }
+    // adds vertex element to array representation
+    void addVertex( Vertex* inVertex );
+    
+    // returns vertex object from index in array rep
+    Vertex* getVertex( int index );
+    
+    // returns array index of target vertex object 
+    int getVertexIndex( std::string name );
+    
+    // navigates graph for shortest path from start and end verticies passesd as arguments
+    void dijkstrasAlgorithm ( std::string startVertex, std::string endVertex );
+}; // END graphDW
 
-    // parameterized constuctor
-    Edge( std::string src, std::string dst, int wt ) {
-        source = src;
-        destination = dst;
-        weight = wt;
-    }
-
-    // assignment operator 
-    void operator=( Edge* e2 ) {
-        source = e2->source;
-        destination = e2->destination;
-        weight = e2->weight;
-    }
-}; // END of Edge struct 
-
-
-// BEGIN VertexTrack struct
-
-struct VertexTrack {     
-    std::string name;    // tag for tracked vertex
-    int targettings;     // count of In-Degrees
-
-    // parameterized constuctor
-    VertexTrack( std::string n, int t ) {
-        name = n;
-        targettings = t;
-    }
-}; // END of VertexTrack struct
-
-
-// BEGIN Vertex struct
-
-struct Vertex {
-    // attributes
-    std::string id;        // tag for vertex id 
-    int outDegree;         // number of destinations from vertex
-    Edge** edges;          // dynamic array of pointers to outbound edges
-
-    bool seen;             // set to false after graph construction
-    int distance;          // distance from desired path source
-    Vertex* lastJump;      // In-Degree vertex last used in processing
-
-    // Vertex() {}    // prolly keep this out right? 
-
-    // parameterized constuctor
-    Vertex( int outDeg, std::string name ) {
-        id = name;                  
-        outDegree = outDeg;
-        edges = new Edge*[outDeg];
-
-        seen = false;
-        distance = 2147483647;       // max int u know just in case wanna have 2147483646 connections
-        lastJump = nullptr;
-    }
-}; // END of Vertex struct
-
-// END GRAPH COMPONENTS 
+// END GRAPH BUILD AND OPERATION
