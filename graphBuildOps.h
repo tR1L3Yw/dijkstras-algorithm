a203e5dcff39387bb63dbcd053e82f0b7e6c6881
tR1L3Yw <163371417+tR1L3Yw@users.noreply.github.com>
2024-03-24 15:22:48 -0500

    fresh graphBuildOps.h

--- graphBuildOps.h
+++ graphBuildOps.h
@@ -1,38 +1,33 @@
-#include "graphComp.h"
-#include "MinHeap.h"
+#include "graphComp.cpp"
+#include "MinHeap.cpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <climits>
 
-#ifndef GRAPHBUILDOPS_H
-#define GRAPHBUILDOPS_H
-
-// BEGIN GRAPH BUILD AND OPERATION
-// - single class graphDW (Directed + Weighted graph)
-// - graph abstraction achieved via dynamic array of Vertex (graphComp) pointers
 
 class graphDW {
 private:
-    
-    int numVertices;
-    int buildIndex;
-    Vertex** graphVertices;
+    // attributes
+    int numVertices;        // vertex count
+    int buildIndex;         // vertex track relative to graph
+    Vertex** graphVertices; // dynamic array representing graph via Vertex pointers
     
 public:
-    
+    // parameterized constructor
     graphDW( int nV );
-    
+
+    // adds vertex element to array representation
     void addVertex( Vertex* inVertex );
     
+    // returns vertex object from index in array rep
     Vertex* getVertex( int index );
     
+    // returns array index of target vertex object 
     int getVertexIndex( std::string name );
     
+    // navigates graph for shortest path from start and end verticies passesd as arguments
     void dijkstrasAlgorithm ( std::string startVertex, std::string endVertex );
-    
-};
+}; // END graphDW
 
 // END GRAPH BUILD AND OPERATION
-
-#endif
